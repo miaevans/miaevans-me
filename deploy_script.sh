1150a22437288553bb39c5e028783de498506ed4
Mia Evans <miaevans@edwin-evanss-macbook-pro.local>
2018-03-12 18:33:03 -0700

    misc

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "misc"
 git push
-ssh ubuntu@ec2-35-163-48-70.us-west-2.compute.amazonaws.com '. /home/ubuntu/simplewebserver/unsafe_deploy_miaevans.sh'
+ssh ubuntu@ec2-54-201-6-120.us-west-2.compute.amazonaws.com '. /home/ubuntu/simplewebserver/unsafe_deploy_miaevans.sh'
